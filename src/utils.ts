a7f5702fb7d86064e96ab409b67441d3da0611be
Parth Tank <parthtank301@gmail.com>
2025-03-29 17:16:20 +0530

    first commit

---
+++ src/utils.ts
@@ -0,0 +1,16 @@
+export function formatDate(date: Date) {
+  const option: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
+
+  return new Date(date).toLocaleDateString(undefined, option);
+}
+
+export function capatalize(str: string): string {
+  if (typeof str !== 'string' || str.length === 0) {
+    return str;
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
